2d33a5197b18a9edb3d63b3ae54c5f70dfe3c639
Code Sandbox <hello@codesandbox.io>
2021-09-18 02:38:28 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f512ecaeb97442b2d52d3fa682ff947d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f03a8a2536f223e6f34a1facd73fb0c",
+    "url": "https://.github.io/csb-tfkz5/index.html"
+  },
+  {
+    "revision": "735236996553e5e08c4b",
+    "url": "https://.github.io/csb-tfkz5/static/css/2.495e36e2.chunk.css"
+  },
+  {
+    "revision": "735236996553e5e08c4b",
+    "url": "https://.github.io/csb-tfkz5/static/js/2.b353c5d7.chunk.js"
+  },
+  {
+    "revision": "41bb7b9f42c95c5a48c1",
+    "url": "https://.github.io/csb-tfkz5/static/js/main.d53fc962.chunk.js"
+  },
+  {
+    "revision": "e5b197f5d1edc8e9b2ab",
+    "url": "https://.github.io/csb-tfkz5/static/js/runtime~main.239bfbf5.js"
+  }
+]);
